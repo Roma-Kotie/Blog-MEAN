9d08dd06da1d6747b47490fb45def1eaafb5c75a
Roman <romanmelnikov221@gmail.com>
2023-10-14 14:37:01 +0300

    basic server logic

--- index.js
+++ index.js
@@ -4,26 +4,38 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const passport = require('passport');
 const path = require('path');
-const config = require('./config/db')
+const config = require('./config/db');
 const app = express();
+const account = require('./routes/account');
 
-const port = 3000;
+const PORT = 3000;
 
-mongoose.connect(config.db);
+app.use(passport.initialize());
+app.use(passport.session());
 
-mongoose.connection.on('conected', () => {
-    console.log("Successful connection to the database")
-});
+require('./config/passport')(passport);
 
-mongoose.connection.on('error', (err) => {
-    console.log(`No successful connection to the database ${err}`)
-});
+app.use(cors());
 
-app.listen(port, () => {
-    console.log( `The server is running, port ${port}`);
+app.use(bodyParser.json());
+
+// mongoose.connect(config.db);
+
+// mongoose.connection.on('connected', () => {
+//     console.log("Successful connection to the database")
+// });
+
+// mongoose.connection.on('error', (err) => {
+//     console.log(`No successful connection to the database ${err}`)
+// });
+
+app.listen(PORT, () => {
+    console.log( `The server is running, port ${PORT}`);
 });
 
 app.get('/', (req, res ) => {
     res.send("Home")
 });
+
+app.use('/account', account);
 
