0e65b206284bbb075f2b528addd207a0bfbaf17e
Roma-Kotie <romanmelnikov221@gmail.com>
2023-11-30 11:20:47 +0200

    add minor adjustments

--- Frontend/src/app/dashboard/dashboard.component.ts
+++ Frontend/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,13 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
+
+  constructor( 
+    private authService: AuthService,
+    private router: Router,
+    ) {}
+
   post = {
     title: '',
     text: '',
-    photo: ''
+    photo: '',
+    //autor: localStorage.getItem("user.name"),
+    //date: new Date()
   };
   
   createPost() {
-    console.log("Create post")
+
+    if (this.post.photo && this.post.text && this.post.title) {
+      // Run logic here to send data
+      console.log('Sending data:', this.post);
+      this.router.navigate(['']);
+    }
+    return false
   };
+    
+  
 }
